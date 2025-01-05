caee7b355d3f12fd0f26f8472111eff2ac189ad2
Kuber Goel <kubergoel@Kubers-MacBook-Air.local>
2025-01-05 13:34:02 +0530

    Adding test cases for fail

--- test_main.py
+++ test_main.py
@@ -38,3 +38,14 @@ def test_auth(client):
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+
+def test_auth_fail(client):
+    body = {'email': EMAIL,
+            'password': PASSWORD}
+    response = client.post('/auth', 
+                           data=json.dumps(body),
+                           content_type='application/json')
+
+    assert response.status_code == 200
+    token = response.json['token']
+    assert token is None
